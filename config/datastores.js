06cefb65e3a3106e8736bcb4e812b2eb7a6c53b5
Rodrigo Oliveira <rodrigo.oliveira@trigg.com.br>
2018-04-18 14:43:03 -0300

    alterando url do bd

--- config/datastores.js
+++ config/datastores.js
@@ -49,7 +49,8 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mongo',
-    url: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/emojis',
+    // url: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/emojis',
+    url: 'mongodb://heroku_8hdjrm0m:rmfourc7as5au1dsf4mam1amit@ds247449.mlab.com:47449/heroku_8hdjrm0m',
   },
 
   // redis: {
